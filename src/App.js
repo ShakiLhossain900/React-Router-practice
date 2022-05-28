cff1d660d61d3d154d5d95716cc7b596ae7db6d2
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-05-28 09:02:34 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./About";
+import React from "react";
+import "./App.css";
+import Nav from "./Nav";
+import Shop from "./Shop";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav >
+            <h3>logo</h3>
+
+            <ul className="nav-links">
+                <li>About</li>
+                <li>Shop</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
