a8bc87f65f87c175c0697bf1ce4725ca4d68356a
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-05-29 22:10:35 +0600

    adding something new

--- src/Nav.js
+++ src/Nav.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
+import './Nav.css'
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-    return (
-        <nav >
-            <h3>logo</h3>
+  return (
+    <nav >
+      <h3>logo</h3>
 
-            <ul className="nav-links">
-                <li>About</li>
-                <li>Shop</li>
-            </ul>
-        </nav>
-    );
+      <ul className="nav-links">
+        <Link to='/about' className='nav-style'>
+          <li>About</li>
+        </Link>
+        <Link to='./shop' className='nav-style'>
+          <li>Shop</li>
+        </Link>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
